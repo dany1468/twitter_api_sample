dfd0c3a2c1f88c1982d867bb44d5ab0216d097cf
dany1468 <dany1468@gmail.com>
2015-03-12 22:39:03 +0900

    Implement client setting
    example:
    $ bundle console
    > $client.search("#ruby -rt", lang: "ja").first.text

--- twitter_crawler.rb
+++ twitter_crawler.rb
@@ -1,1 +1,8 @@
 require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ''
+  config.consumer_secret = ''
+  config.access_token = ''
+  config.access_token_secret = ''
+end
